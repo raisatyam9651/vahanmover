6ab4c99bf07a7a4e8b612976e29edf2fa854900d
BP <bp@BPs-MacBook-Air.local>
2026-01-28 21:39:25 +0530

    Format Google Analytics code in header

--- includes/header-link.php
+++ includes/header-link.php
@@ -3,6 +3,15 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <!-- SEO Meta Tags -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8KN112RYKL"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-8KN112RYKL');
+</script>
 <meta name="google-site-verification" content="xjeLREcMez2kegDf2QsMgGLabvzbJVgrIwn4XZR5R-c" />
 <link rel="canonical" href="<?php echo 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" />
 <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
